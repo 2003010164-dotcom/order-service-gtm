40f5d412ad580ae91a2003084f356575226b0d65
2003010164-dotcom <2003010164@ipec.org.in>
2025-10-09 22:19:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
