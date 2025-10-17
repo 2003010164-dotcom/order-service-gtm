8b973c3ec5edb48f2b03af66db4e04582fb538b8
2003010164-dotcom <2003010164@ipec.org.in>
2025-10-17 15:31:13 +0530

    recently Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
